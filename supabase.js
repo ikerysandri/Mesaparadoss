2b8d191b5cb0d87c3687bb5003a5391972027087
ikerysandri <iiikeeergl@gmail.com>
2026-05-22 00:30:46 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
-const supabaseUrl = import.meta.env.SUPABASE_URL
-const supabaseKey = import.meta.env.API_KEY
+const supabaseUrl = import.meta.env.VITE_SUPABASE_URL
+const supabaseKey = import.meta.env.VITE_API_KEY
 
 export const supabase = createClient(supabaseUrl, supabaseKey)
